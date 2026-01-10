6896fb9d7673231e89cc28542b9c5209b8dde3ed
Sven Strohschein <sven.strohschein@gmail.com>
2026-01-10 23:21:27 +0100

    gradle-compile-java-issue
    - hotswap-agent-tryout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,6 +26,7 @@ public class Main {
 
             System.out.println("EnhancedPOJO version: " + enhancedPojo.getVersion());
             System.out.println("EnhancedPOJO enhanced: " + enhancedPojo.isEnhanced());
+            System.out.println("EnhancedPOJO method number: " + EnhancedPOJO.class.getMethods().length);
 
             Thread.sleep(2000L);
         } while(true);
@@ -48,8 +49,8 @@ public class Main {
                 .intercept(FixedValue.value(true))
 //                // optional: when this is activated, IntelliJ throws an error at 'Apply HotSwap'
 //                // because the JRE (jbr / dcevm) doesn't support reloading classes when number of methods gets changed.
-//                .defineMethod("addedMethod", boolean.class, Modifier.PUBLIC)
-//                .intercept(FixedValue.value(true))
+                .defineMethod("addedMethod", boolean.class, Modifier.PUBLIC)
+                .intercept(FixedValue.value(true))
                 .make()) {
             enhancedType.load(classLoader, ClassLoadingStrategy.Default.INJECTION);
         }
--- src/main/java/org/example/hotswap/ByteCodeEnhancementHotswapPlugin.java
+++ src/main/java/org/example/hotswap/ByteCodeEnhancementHotswapPlugin.java
@@ -12,12 +12,19 @@ public class ByteCodeEnhancementHotswapPlugin {
 
     @OnClassLoadEvent(classNameRegexp = ".*", events = LoadEvent.REDEFINE)
     public static void patch(final CtClass ctClass, final ClassLoader classLoader, final Class<?> originalClass) throws IOException, CannotCompileException, NotFoundException {
-        System.out.println("patch ByteCodeEnhancementHotswapPlugin");
-    }
+        System.out.println("ByteCodeEnhancementHotswapPlugin: patch class " + originalClass.getName());
 
-    @OnClassLoadEvent(classNameRegexp = ".*", events = LoadEvent.REDEFINE)
-    public void onClassLoad() {
-        System.out.println("onClassLoad");
+        if("org.example.enhanced.EnhancedPOJO".equals(originalClass.getName())) {
+            System.out.println("original methods: " + originalClass.getMethods().length);
+            try {
+                Class<?> alreadyEnhancedClass = classLoader.loadClass("org.example.enhanced.EnhancedPOJO");
+                System.out.println("org.example.enhanced.EnhancedPOJO enhanced found? " + (alreadyEnhancedClass != null));
+
+                System.out.println("alreadyEnhancedClass methods: " + alreadyEnhancedClass.getMethods().length);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @OnClassFileEvent(classNameRegexp = ".*")
