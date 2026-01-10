bc99d8cd5f44b4b4ae41c1b763914443d2fd490d
Sven Strohschein <sven.strohschein@gmail.com>
2026-01-10 22:59:39 +0100

    gradle-compile-java-issue
    - hotswap-agent-tryout

---
+++ src/main/java/org/example/hotswap/ByteCodeEnhancementHotswapPlugin.java
@@ -0,0 +1,41 @@
+package org.example.hotswap;
+
+import org.hotswap.agent.annotation.*;
+import org.hotswap.agent.javassist.CannotCompileException;
+import org.hotswap.agent.javassist.CtClass;
+import org.hotswap.agent.javassist.NotFoundException;
+
+import java.io.IOException;
+
+@Plugin(name = "ByteCodeEnhancement", testedVersions = {"DCEVM"})
+public class ByteCodeEnhancementHotswapPlugin {
+
+    private static final String HOTSWAP_AGENT_CLINIT_METHOD = "$$ha$clinit";
+
+    public static boolean reloadFlag;
+
+    @OnClassLoadEvent(classNameRegexp = ".*", events = LoadEvent.REDEFINE)
+    public static void patch(final CtClass ctClass, final ClassLoader classLoader, final Class<?> originalClass) throws IOException, CannotCompileException, NotFoundException {
+        System.out.println("patch ByteCodeEnhancementHotswapPlugin");
+    }
+
+    @Init
+    public static void init() {
+        System.out.println("My HotswapAgent plugin loaded");
+    }
+
+    @OnClassLoadEvent(classNameRegexp = ".*", events = LoadEvent.REDEFINE)
+    public void onClassLoad() {
+        System.out.println("onClassLoad");
+    }
+
+    @OnClassFileEvent(classNameRegexp = ".*")
+    public void onClassFile() {
+        System.out.println("onClassFile");
+    }
+
+    @OnResourceFileEvent(path = "/")
+    public void onResourceChanged() {
+        System.out.println("onResourceChanged");
+    }
+}
